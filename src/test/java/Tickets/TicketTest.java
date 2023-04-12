ad4b9b281048c27ecbb2ca752b89dc74e2d81c87
Fikret <tardileo1996@gmail.com>
2023-04-12 23:53:03 +0300

    Second Commit

--- src/test/java/Tickets/TicketTest.java
+++ src/test/java/Tickets/TicketTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 class TicketTest {
 
     @Test
-    public void TestSortTickets() {
+    public void TestSortTicket() {
         TicketRepository repo = new TicketRepository();
         TicketManager manager = new TicketManager(repo);
         Ticket ticket1 = new Ticket(1, "MSK", "SPB", 950, 93);
@@ -29,6 +29,93 @@ class TicketTest {
 
         Ticket[] expected = {ticket6, ticket1, ticket2, ticket3, ticket8, ticket5};
         Ticket[] actual = manager.findAll("MSK", "SPB");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindOneTicket() {
+        TicketRepository repo = new TicketRepository();
+        TicketManager manager = new TicketManager(repo);
+        Ticket ticket1 = new Ticket(1, "MSK", "SPB", 950, 93);
+        Ticket ticket2 = new Ticket(2, "MSK", "SPB", 1000, 93);
+        Ticket ticket3 = new Ticket(3, "MSK", "SPB", 1000, 93);
+        Ticket ticket4 = new Ticket(4, "OMSK", "MSK", 2000, 102);
+        Ticket ticket5 = new Ticket(5, "MSK", "SPB", 1500, 93);
+        Ticket ticket6 = new Ticket(6, "MSK", "SPB", 900, 93);
+        Ticket ticket7 = new Ticket(7, "LA", "NY", 10000, 298);
+        Ticket ticket8 = new Ticket(8, "MSK", "SPB", 1000, 99);
+
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+        manager.add(ticket5);
+        manager.add(ticket6);
+        manager.add(ticket7);
+        manager.add(ticket8);
+
+        Ticket[] expected = {ticket7};
+        Ticket[] actual = manager.findAll("LA", "NY");
+
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindFewTickets() {
+        TicketRepository repo = new TicketRepository();
+        TicketManager manager = new TicketManager(repo);
+        Ticket ticket1 = new Ticket(1, "MSK", "SPB", 950, 93);
+        Ticket ticket2 = new Ticket(2, "MSK", "SPB", 1000, 93);
+        Ticket ticket3 = new Ticket(3, "MSK", "SPB", 1000, 93);
+        Ticket ticket4 = new Ticket(4, "OMSK", "MSK", 2000, 102);
+        Ticket ticket5 = new Ticket(5, "MSK", "SPB", 1500, 93);
+        Ticket ticket6 = new Ticket(6, "MSK", "SPB", 900, 93);
+        Ticket ticket7 = new Ticket(7, "LA", "NY", 10000, 298);
+        Ticket ticket8 = new Ticket(8, "OMSK", "MSK", 1000, 99);
+
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+        manager.add(ticket5);
+        manager.add(ticket6);
+        manager.add(ticket7);
+        manager.add(ticket8);
+
+        Ticket[] expected = {ticket8, ticket4};
+        Ticket[] actual = manager.findAll("OMSK", "MSK");
+
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void notFindAnyTicket() {
+        TicketRepository repo = new TicketRepository();
+        TicketManager manager = new TicketManager(repo);
+        Ticket ticket1 = new Ticket(1, "MSK", "SPB", 950, 93);
+        Ticket ticket2 = new Ticket(2, "MSK", "SPB", 1000, 93);
+        Ticket ticket3 = new Ticket(3, "MSK", "SPB", 1000, 93);
+        Ticket ticket4 = new Ticket(4, "OMSK", "MSK", 2000, 102);
+        Ticket ticket5 = new Ticket(5, "MSK", "SPB", 1500, 93);
+        Ticket ticket6 = new Ticket(6, "MSK", "SPB", 900, 93);
+        Ticket ticket7 = new Ticket(7, "LA", "NY", 10000, 298);
+        Ticket ticket8 = new Ticket(8, "OMSK", "MSK", 1000, 99);
+
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+        manager.add(ticket5);
+        manager.add(ticket6);
+        manager.add(ticket7);
+        manager.add(ticket8);
+
+        Ticket[] expected = {};
+        Ticket[] actual = manager.findAll("IST", "KAZ");
+
 
         Assertions.assertArrayEquals(expected, actual);
     }
